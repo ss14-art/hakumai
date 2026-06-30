03629447d9ef875051a515814e4ef8974c0f2d9d
Odleer <odlieerr@gmail.com>
2026-06-30 14:45:36 +0500

    Resprite and fix

--- Content.Shared/Localizations/ContentLocalizationManager.cs
+++ Content.Shared/Localizations/ContentLocalizationManager.cs
@@ -10,8 +10,7 @@ namespace Content.Shared.Localizations
         [Dependency] private readonly ILocalizationManager _loc = default!;
 
         // If you want to change your codebase's language, do it here.
-        private const string Culture = "ru-RU"; // Localization
-        private const string FallbackCulture = "en-US"; // Localization
+        private const string Culture = "en-US";
 
         /// <summary>
         /// Custom format strings used for parsing and displaying minutes:seconds timespans.
@@ -27,11 +26,8 @@ namespace Content.Shared.Localizations
         public void Initialize()
         {
             var culture = new CultureInfo(Culture);
-			var fallbackCulture = new CultureInfo(FallbackCulture); // Localization
 
             _loc.LoadCulture(culture);
-            _loc.LoadCulture(fallbackCulture); // Localization
-            _loc.SetFallbackCluture(fallbackCulture); // Localization
             _loc.AddFunction(culture, "PRESSURE", FormatPressure);
             _loc.AddFunction(culture, "POWERWATTS", FormatPowerWatts);
             _loc.AddFunction(culture, "POWERJOULES", FormatPowerJoules);
